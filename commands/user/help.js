e2f48fb5f6dea3be3ccbfa6d476928734977eb65
2LuCas2 <94871219+2LuCas2@users.noreply.github.com>
2021-12-01 18:50:57 +0100

    Update help.js

--- commands/user/help.js
+++ commands/user/help.js
@@ -6,15 +6,17 @@ config: {
         aliases: ['aide'],
 },
 
-    run: async (bot, message, client, args) => {
+    run: async (bot, message) => {
         let embed = new MessageEmbed()
         .setColor(`RED`)
-.setDescription('**Voici la liste de mes commandes (10)**')
+.setDescription('**Voici la liste de mes commandes (15)**')
         .addFields(
 
             { name: `💥・antiraid (2)`, value: '`antibot`, `antiweb`'},
+                
+            { name: `📂・log (1)`, value: '`setwelcome-on/off`',},
 
-            { name: `💰・jeu (1)`, value: '`ppc`',} ,
+            { name: `💰・jeu (1)`, value: '`ppc`',},
 
             { name: `🏠・Utilitaire (6)`, value: '`help`, `speed`, `banlist`, `botlist`, `adminlist`, `suggest`'},
 
